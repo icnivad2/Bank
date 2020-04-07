aab04cf15f6f39e32a7c45af5d67b62c7ff88a41
linuxGuru <linuxguru@linuxguru-HP-ProBook-6470b.attlocal.net>
2020-04-07 14:20:09 -0400

    inital upload

--- Main.java
+++ Main.java
@@ -9,13 +9,13 @@ public class Main {
 		Bank sue = new Bank(10011,10340.34,"Sue","343-2334","sue@mail.com");
 		
 		
-		System.out.println("Account balance: " + sue.getName() + "--"  + sue.getBalance());
+		System.out.println("Acct balance: " + sue.getName() + "--"  + sue.getBalance());
 
-		System.out.println("Account balance: " + joe.getName() + "--"  + joe.getBalance());
+		System.out.println("Acct balance: " + joe.getName() + "--"  + joe.getBalance());
 		
 		System.out.println(joe.withdrawal( 300));
 
-	//	System.out.println("Account balance: " + joe.getName() + "--"  + joe.getBalance());
+	//	System.out.println("At balance: " + joe.getName() + "--"  + joe.getBalance());
 	  System.out.println(joe.withdrawal( 200));
 
 //		System.out.println("Acct balance: " + joe.getName() + "--"  + joe.getBalance());
